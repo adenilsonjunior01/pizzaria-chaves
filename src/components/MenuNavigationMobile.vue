e62746a8b911be94dbdae2398d18d9014cac47bd
Adenilson Souza Santos Junior - AJN <adenilson.souza@iteris.local>
2021-09-03 12:21:58 -0300

    update

--- src/components/MenuNavigationMobile.vue
+++ src/components/MenuNavigationMobile.vue
@@ -3,7 +3,7 @@
     <ul>
       <router-link to="/" tag="li" active-class="active" exact @click="closeMenu()"><a>Início</a></router-link>
       <router-link to="/cardapio" tag="li" active-class="active" @click="closeMenu()"><a>Cardápio</a></router-link>
-      <router-link to="/pizzas" tag="li" active-class="active"><a>Pizzas</a></router-link>
+      <router-link to="/pizzas" tag="li" active-class="active" @click="closeMenu()"><a>Pizzas</a></router-link>
       <router-link to="/contato" tag="li" active-class="active" @click="closeMenu()"><a>Contato</a></router-link>
     </ul>
     <SocialMedia :isVisible="false"/>
