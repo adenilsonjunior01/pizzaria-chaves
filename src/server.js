8d549e67a4cad5935a0e8f5f6b77a7e8121f59b3
Adenilson Souza Santos Junior - AJN <adenilson.souza@iteris.local>
2021-09-03 11:40:22 -0300

    update server.js

--- src/server.js
+++ src/server.js
@@ -1,15 +1,10 @@
-function requireHTTPS(req, res, next) {
-  if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-    return res.redirect('https://' + req.get('host') + req.url);
-  }
-  next();
-}
-const express = require('express');
+import express from "express";
 const app = express();
-app.use(requireHTTPS);
 
-app.use(express.static(`./dist/`));
+app.use(static(__dirname + '/dist'));
 
-app.get('/*', (req, res) => res.sendFile('index.html', { root: 'dist/' }));
+app.all('*', (req, res) => {
+  res.status(200).sendFile(__dirname + '/dist/index.html');
+});
 
 app.listen(process.env.PORT || 8080);
